22034254dc523df6007e6d2792c606898c49c6cf
Salt <luffy.w.pirate@gmail.com>
2018-03-30 07:18:36 +0800

    Fix publicPath

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'nobody-in-the-organization/tips-for-design-frontend/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
